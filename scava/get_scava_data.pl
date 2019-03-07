a58a8faaf2c98ac000019c21564403c7dc25f3f5
Boris Baldassari <boris.baldassari@gmail.com>
2019-03-08 00:51:26 +0100

    #2 Update script, add output example.

--- scava/get_scava_data.pl
+++ scava/get_scava_data.pl
@@ -37,20 +37,28 @@ sub get_url() {
     return $data
 }
 
+sub write_file() {
+    my $json = shift;
+    my $file_out = shift;
+
+    open my $fh, '>', $file_out or die "Cannot open file $file_out.\n";
+    print $fh $json;
+    close $fh;
+}
 
 # Send GET request
 
 print "\n\n";
 print "# FACTOIDS:\n";
 
-my $f = &get_url("http://ci4.castalia.camp:8182/factoids/");
+my $f = &get_url("http://ci4.castalia.camp:8182/factoids/"); 
 my %factoids;
 for my $factoid (@{$f}) {
     $factoids{ $factoid->{'id'} }->{'name'} = $factoid->{'name'};
-    $factoids{ $factoid->{'id'} }->{'description'} = $factoid->{'description'};
+    $factoids{ $factoid->{'id'} }->{'description'} = $factoid->{'summary'};
 }
-
-print Dumper(%factoids);
+my $factoids_json = encode_json(\%factoids);
+&write_file($factoids_json, "factoids.json");
 
 #for my $f (@factoids) {
 #    print "  - " .  . ": " . $f->{'summary'} . "\n";
@@ -66,28 +74,37 @@ for my $metric (@{$m}) {
     $metrics{ $metric->{'id'} }->{'description'} = $metric->{'description'};
 }
 
-print Dumper(%metrics);
-print "\n\n";
+#print Dumper(%metrics);
+#print "\n\n";
 
+my $metrics_json = encode_json(\%metrics);
+&write_file($metrics_json, "metrics.json");
 
 print "# Retrieving list of projects.";
 my $p = &get_url("http://ci4.castalia.camp:8086/administration/projects/");
 my @projects = map { $_->{'shortName'} } @$p;
 
+print Dumper(@projects);
+
+my %projects;
 for my $project (@projects) {
 
     print "# Metrics for project $project\n";
     for my $metric (keys %metrics) {
         print "   - $metric [http://ci4.castalia.camp:8182/projects/p/$project/m/$metric].\n";
         my $m = &get_url("http://ci4.castalia.camp:8182/projects/p/$project/m/$metric");
-        print "     " . Dumper($m) . "\n";
+	$projects{$project} = $m;
+#        print "     " . Dumper($m) . "\n";
     }
 
-    print "# Retrieving similar projects for $project.";
+    print "# Retrieving similar projects for $project.\n";
     my $s = &get_url("http://ci4.castalia.camp:8080/api/recommendation/similar/p/$project/m/Compound/n/3");
-    print Dumper($s);
+#    print Dumper($s);
 
 }
+
+my $projects_json = encode_json(\%projects);
+&write_file($projects_json, "projects.json");
 
 
 
