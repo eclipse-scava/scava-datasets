db94861c988f28ca1ac47f25945858fa2e0d91ae
Boris Baldassari <boris.baldassari@gmail.com>
2019-03-11 11:38:25 +0100

    #3 Update perl get script.

--- scava/get_scava_data.pl
+++ scava/get_scava_data.pl
@@ -17,12 +17,16 @@ use Mojo::UserAgent;
 use Mojo::JSON qw(decode_json encode_json);
 use Data::Dumper;
 
+my $base_url = 'http://ci4.castalia.camp';
+#my $base_url = 'http://scava-dev.ow2.org';
+
 # Prepare for http requests
 my $ua  = Mojo::UserAgent->new;
 
 sub get_url() {
     my $url_in = shift;
 
+    print "* Fetching ${url_in}.\n";
     my $res = $ua->get($url_in)->result;
 
     if (not $res->is_success) {
@@ -32,6 +36,9 @@ sub get_url() {
     }
 
     # Decode JSON from server
+    my $file_in = $url_in;
+    $file_in =~ s!/!_!g;
+#    &write_file($res->body, $file_in . '.json');
     my $data = decode_json($res->body);
 
     return $data
@@ -51,7 +58,7 @@ sub write_file() {
 print "\n\n";
 print "# FACTOIDS:\n";
 
-my $f = &get_url("http://ci4.castalia.camp:8182/factoids/"); 
+my $f = &get_url($base_url . ":8182/factoids/"); 
 my %factoids;
 for my $factoid (@{$f}) {
     $factoids{ $factoid->{'id'} }->{'name'} = $factoid->{'name'};
@@ -67,7 +74,7 @@ my $factoids_json = encode_json(\%factoids);
 print "\n\n";
 print "# METRICS:\n";
 
-my $m = &get_url("http://ci4.castalia.camp:8182/metrics/");
+my $m = &get_url($base_url . ":8182/metrics/");
 my %metrics; 
 for my $metric (@{$m}) {
     $metrics{ $metric->{'id'} }->{'name'} = $metric->{'name'};
@@ -81,24 +88,48 @@ my $metrics_json = encode_json(\%metrics);
 &write_file($metrics_json, "metrics.json");
 
 print "# Retrieving list of projects.";
-my $p = &get_url("http://ci4.castalia.camp:8086/administration/projects/");
+my $p = &get_url($base_url . ":8086/administration/projects/");
 my @projects = map { $_->{'shortName'} } @$p;
 
 print Dumper(@projects);
 
 my %projects;
+my $projects_csv = "project,id,name,type,x,y,value_x,value_y\n";
+
 for my $project (@projects) {
 
     print "# Metrics for project $project\n";
     for my $metric (keys %metrics) {
-        print "   - $metric [http://ci4.castalia.camp:8182/projects/p/$project/m/$metric].\n";
-        my $m = &get_url("http://ci4.castalia.camp:8182/projects/p/$project/m/$metric");
+        print "   - $metric [" . $base_url . ":8182/projects/p/$project/m/$metric].\n";
+        my $m = &get_url($base_url . ":8182/projects/p/$project/m/$metric");
+	# populate out json
 	$projects{$project} = $m;
+	# populate out csv
+	if ( scalar( @{$m->{'datatable'}} ) > 0 ) {
+	    for my $d ( @{$m->{'datatable'}} ) {
+#		print "DBG " . Dumper($d) . "\n";
+		$projects_csv = "$project,";
+		$projects_csv .= $m->{'id'} . ",";
+		$projects_csv .= $m->{'name'} . ",";
+		$projects_csv .= $m->{'type'} . ",";
+		$projects_csv .= $m->{'x'} . ",";
+		$projects_csv .= $m->{'y'} . ",";
+		$projects_csv .= $d->{'Date'} . ",";
+		$projects_csv .= $d->{'Val'} . "\n";
+	    }
+	} else {
+	    $projects_csv = "$project,";
+	    $projects_csv .= $m->{'id'} . ",";
+	    $projects_csv .= $m->{'name'} . ",";
+	    $projects_csv .= $m->{'type'} . ",";
+	    $projects_csv .= $m->{'x'} . ",";
+	    $projects_csv .= $m->{'y'} . ",,\n";
+	}
 #        print "     " . Dumper($m) . "\n";
     }
 
-    print "# Retrieving similar projects for $project.\n";
-    my $s = &get_url("http://ci4.castalia.camp:8080/api/recommendation/similar/p/$project/m/Compound/n/3");
+#    print "# Retrieving similar projects for $project.\n";
+#    my $s = &get_url($base_url . "/api/recommendation/similar/p/$project/m/Compound/n/3");
 #    print Dumper($s);
 
 }
